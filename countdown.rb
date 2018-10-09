1e80e19adb00623a9a000b01b172410c8830471c
anik1606 <anik1606@gmail.com>
2018-10-09 18:54:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,11 @@ return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(n)
+    while integer > 0 
+  puts "#{integer} SECOND(S)!"
   sleep(1)
+  integer -= 1 
+end
+return "HAPPY NEW YEAR!"
+  
 end
