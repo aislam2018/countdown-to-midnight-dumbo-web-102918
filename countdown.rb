ca0439e800268989ed7e88be0d5aafb136fbd3a9
anik1606 <anik1606@gmail.com>
2018-10-09 18:52:44 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 
 
 def countdown(integer)
-  while x > 0 
-  puts "#{x} SECOND(S)!"
-  x -= 1 
+  while integer > 0 
+  puts "#{integer} SECOND(S)!"
+  integer -= 1 
 end
 return "HAPPY NEW YEAR!"
 end
